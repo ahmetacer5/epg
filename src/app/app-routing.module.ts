747930e2b7b2db115dfc1f05a0daec726c0a7f27
acer <dev>
2019-04-10 21:19:30 +0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { ChannelsViewComponent } from './views/channels-view/channels-view.component';
+import { DetailLayoutComponent } from './layouts/detail-layout/detail-layout.component';
+import { ProgramViewComponent } from './views/program-view/program-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent,
+    children: [
+      {path: '', redirectTo: 'channels', pathMatch: 'full'},
+      {path: 'channels', component: ChannelsViewComponent}
+    ]
+  },
+  {
+    path: 'program', component: DetailLayoutComponent,
+    children: [
+      {path: ':program_id', component: ProgramViewComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,62 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ChannelsViewComponent } from './views/channels-view/channels-view.component';
+import { TimeLineComponent } from './components/time-line/time-line.component';
+import { DateSelectorComponent } from './components/time-line/date-selector/date-selector.component';
+import { DateSelectorItemComponent } from './components/time-line/date-selector/date-selector-item/date-selector-item.component';
+import { ChannelListComponent } from './components/time-line/channel-list/channel-list.component';
+import { ChannelListItemComponent } from './components/time-line/channel-list/channel-list-item/channel-list-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ScheduleListComponent } from './components/time-line/channel-list/channel-list-item/schedule-list/schedule-list.component';
+import { ScheduleListItemComponent } from './components/time-line/channel-list/channel-list-item/schedule-list/schedule-list-item/schedule-list-item.component';
+import { TimeRowComponent } from './components/time-line/time-row/time-row.component';
+import { FixScheduleScrollingDirective } from './directives/fix-schedule-scrolling.directive';
+import { BlockResizerDirective } from './directives/block-resizer.directive';
+import { CurrentTimeIndicatorComponent } from './components/time-line/current-time-indicator/current-time-indicator.component';
+import { RealTimeClockDirective } from './directives/real-time-clock.directive';
+import { NowButtonComponent } from './components/now-button/now-button.component';
+import { DetailLayoutComponent } from './layouts/detail-layout/detail-layout.component';
+import { ProgramViewComponent } from './views/program-view/program-view.component';
+import { ProgramTitleComponent } from './components/program-title/program-title.component';
+import { ProgramDescriptionComponent } from './components/program-description/program-description.component';
+import { ProgramSeriesComponent } from './components/program-series/program-series.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainLayoutComponent,
+    HeaderComponent,
+    ChannelsViewComponent,
+    TimeLineComponent,
+    DateSelectorComponent,
+    DateSelectorItemComponent,
+    ChannelListComponent,
+    ChannelListItemComponent,
+    ScheduleListComponent,
+    ScheduleListItemComponent,
+    TimeRowComponent,
+    FixScheduleScrollingDirective,
+    BlockResizerDirective,
+    CurrentTimeIndicatorComponent,
+    RealTimeClockDirective,
+    NowButtonComponent,
+    DetailLayoutComponent,
+    ProgramViewComponent,
+    ProgramTitleComponent,
+    ProgramDescriptionComponent,
+    ProgramSeriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
